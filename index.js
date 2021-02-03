ae796b2ec35d1a3faa10ddb64ccdd57f58264213
otkrickey <rtk.otk@gmail.com>
2021-02-04 07:41:31 +0900

    tsc output

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = __importDefault(require("discord.js"));
 const client = new discord_js_1.default.Client();
 require('dotenv').config();
+const token = process.env.TOKEN;
+const prefix = process.env.PREFIX;
 const command = (client, aliases, callback) => {
     const _aliases = (typeof aliases === 'string') ? [aliases] : aliases;
     client.on('message', function (message) {
         const { content } = message;
         _aliases.forEach(function (alias) {
-            const command = `${process.env.PREFIX}${alias}`;
+            const command = `${prefix}${alias}`;
             if (content.startsWith(`${command} `) || content === command) {
                 console.log(`Running the command ${command}`);
                 callback(message);
@@ -21,8 +23,24 @@ const command = (client, aliases, callback) => {
 };
 client.on('ready', function () {
     console.log('The Client is ready');
-    command(client, 'ping', function (message) {
+    command(client, ['ping', 'test'], function (message) {
         message.channel.send('Pong!');
     });
+    command(client, 'servers', function (message) {
+        client.guilds.cache.forEach(function (guild) {
+            message.channel.send(`${guild.name} has a total of ${guild.memberCount}`);
+        });
+    });
+    command(client, ['cc', 'clearChannel'], function (message) {
+        if (message.member?.hasPermission('ADMINISTRATOR')) {
+            message.channel.messages.fetch().then((results) => {
+                message.channel.bulkDelete(results);
+            });
+        }
+    });
+    command(client, 'status', function (message) {
+        const content = message.content.replace('!status', '');
+        client.user?.setPresence({ activity: { name: content, type: 0 } });
+    });
 });
-client.login(process.env.TOKEN);
+client.login(token);
