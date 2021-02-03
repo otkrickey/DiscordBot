86afe486d4eca0a9aeecf0e6dea2074906e6444a
otkrickey <rtk.otk@gmail.com>
2021-02-04 07:41:13 +0900

    Add some functions

--- src/index.ts
+++ src/index.ts
@@ -21,10 +21,57 @@ const command = (client: Discord.Client, aliases: string | string[], callback: (
     });
 }
 
+
+const addReactions = (message: Discord.Message, reactions: string[]) => {
+    message.react(reactions[0]);
+    reactions.shift();
+    if (reactions.length > 0) {
+        setTimeout(() => addReactions(message, reactions), 750);
+    }
+}
+
+
+// const first_message = async (client: Discord.Client, id: string | number, text: string, reactions?: string[] = []) => {
+//     const _id = (typeof id === 'string') ? id : String(id);
+//     const channel = await client.channels.fetch('');
+//     channel.messages.fetch().then((messages) => {
+//         if (messages.size === 0) {
+//             channel.send(text).then(message => {
+//                 addReactions(message, reactions);
+//             });
+//         } else {
+
+//         }
+//     });
+// }
+
+
+
+
 client.on('ready', function () {
     console.log('The Client is ready');
-    command(client, 'ping', function (message) {
+
+    command(client, ['ping', 'test'], function (message) {
         message.channel.send('Pong!');
+    });
+
+    command(client, 'servers', function (message) {
+        client.guilds.cache.forEach(function (guild) {
+            message.channel.send(`${guild.name} has a total of ${guild.memberCount}`);
+        });
+    });
+
+    command(client, ['cc', 'clearChannel'], function (message) {
+        if (message.member?.hasPermission('ADMINISTRATOR')) {
+            message.channel.messages.fetch().then((results) => {
+                (message.channel as Discord.TextChannel | Discord.NewsChannel).bulkDelete(results);
+            });
+        }
+    });
+
+    command(client, 'status', function (message) {
+        const content = message.content.replace('!status', '');
+        client.user?.setPresence({ activity: { name: content, type: 0 } });
     });
 });
 
