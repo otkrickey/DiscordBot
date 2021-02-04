d940623636dbff4ff35f4f013d1ead13cb82baf3
otkrickey <rtk.otk@gmail.com>
2021-02-04 17:50:36 +0900

    Add Voting function

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ const token = process.env.TOKEN
 const prefix = process.env.PREFIX
 
 
+import emoji from 'node-emoji';
+
+const EMOJI = (text: string | undefined): string | undefined => {
+    const _c = text ? emoji.find(text) : undefined
+    return _c ? _c.emoji : undefined
+}
+
 const command = (client: Discord.Client, aliases: string | string[], callback: (message: Discord.Message) => void): void => {
     const _aliases = (typeof aliases === 'string') ? [aliases] : aliases;
 
@@ -22,30 +29,31 @@ const command = (client: Discord.Client, aliases: string | string[], callback: (
 }
 
 
-const addReactions = (message: Discord.Message, reactions: string[]) => {
-    message.react(reactions[0]);
-    reactions.shift();
+const AddReactions = (message: Discord.Message, reactions: string[]): void => {
+    const reaction = EMOJI(reactions.shift());
+    if (reaction) { message.react(reaction); }
     if (reactions.length > 0) {
-        setTimeout(() => addReactions(message, reactions), 750);
+        return AddReactions(message, reactions);
     }
 }
 
-
-const first_message = async (client: Discord.Client, id: string | number, text: string, reactions?: string[] = []) => {
+const StartVoting = async (client: Discord.Client, id: string | number, text: string, reactions: string[] = []) => {
     const _id = (typeof id === 'string') ? id : String(id);
-    const channel = client.channels.cache.get(_id);
-    channel.messages.fetch().then((messages) => {
-        if (messages.size === 0) {
-            channel.send(text).then(message => {
-                addReactions(message, reactions);
-            });
-        } else { }
-    });
+    const _channel = await client.channels.fetch(_id);
+    const channel = (_channel as Discord.TextChannel);
+    channel.send(`[vote] "${text}"`).then(message => { AddReactions(message, reactions); });
 }
 
 
 client.on('ready', function () {
     console.log('The Client is ready');
+
+    command(client, 'vote', function (message) {
+        const content = message.content.replace('!vote ', '');
+        const Emojis = ['o', 'x'];
+        message.delete();
+        StartVoting(client, '805560753657479231', content, Emojis);
+    });
 
     command(client, ['ping', 'test'], function (message) {
         message.channel.send('Pong!');
