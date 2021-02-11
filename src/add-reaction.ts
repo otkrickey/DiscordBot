48cced6cd86decca1f014ef16a9375a17c2244a3
otkrickey <rtk.otk@gmail.com>
2021-02-11 17:14:31 +0900

    Moved the AddrReaction function to another file

---
+++ src/add-reaction.ts
@@ -0,0 +1,18 @@
+import Discord = require('discord.js');
+import emoji = require('node-emoji');
+require('dotenv').config();
+
+
+
+function addReactions(message: Discord.Message, reactions: string[]): void {
+    const reaction = reactions.shift();
+    if (reaction) {
+        const Emoji = emoji.get(reaction);
+        message.react(Emoji);
+    }
+    if (reactions.length) {
+        return addReactions(message, reactions);
+    }
+}
+
+export default addReactions
